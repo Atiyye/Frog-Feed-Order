e9f4a45eb6333f3dd730b981e12f72bacb7ec2ec
Atiyye <atiyyecerkeslioglu@gmail.com>
2024-11-27 13:59:52 +0300

    Scriptable Objects added

---
+++ FrogFeedOrder/Assets/Scripts/Core/Board.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private Flexalon.FlexalonGridLayout Grid;
+
+    public void Start()
+    {
+    }
+}
---
+++ FrogFeedOrder/Assets/Scripts/SO/TileStateSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Tile State")] 
+public class TileStateSO : ScriptableObject
+{
+    public Material tileMaterial;
+    public Material objectMaterial;
+    public GameObject tileContent;
+}
